40652f4bcf10c9d53713a974309c10d51cabc11d
ext_vmatveev <vmatveyev@neoflex.ru>
2021-08-06 22:14:17 +0300

    FLUMEN-1. Got around compiler issue with path dependent types https://github.com/scala/bug/issues/5712

--- src/main/scala/ru/neoflex/flumen/ServiceRouter.scala
+++ src/main/scala/ru/neoflex/flumen/ServiceRouter.scala
@@ -1,22 +1,23 @@
 package ru.neoflex.flumen
 
 sealed trait ContrServiceList[A]
-final case class ::=>[A, H, T <: ContrServiceList[A]](head : Service[A,H], tail : T) extends ContrServiceList[A] {
-}
-sealed class ContrNil[A] extends ContrServiceList[A] {
-}
+
+sealed class ContrNil[A] extends ContrServiceList[A]
+
 object ContrNil {
   def apply[A] = new ContrNil[A]
 }
 
+final case class ContrCompose[A, H, T <: ContrServiceList[A]](head : Service[A,H], tail : T) extends ContrServiceList[A] {
+}
+
 object ContrServiceList{
-  def apply[A,H](service: Service[A,H]) = {::=>[A, H,ContrNil[A]](service,  ContrNil[A])}
+  def apply[A,H](service: Service[A,H]) = {ContrCompose[A, H,ContrNil[A]](service,  ContrNil[A])}
 }
 
 
 sealed trait CovServiceList[A]
-final case class ::<=[H, A, T <: CovServiceList[A]](head : Service[H,A], tail : T) extends CovServiceList[A] {
-}
+
 sealed class CovNil[A] extends CovServiceList[A] {
 }
 
@@ -24,8 +25,11 @@ object CovNil {
   def apply[A] = new CovNil[A]
 }
 
+final case class CovCompose[H, A, T <: CovServiceList[A]](head : Service[H,A], tail : T) extends CovServiceList[A] {
+}
+
 object CovServiceList {
-  def apply[H,A](service: Service[H,A]) ={::<=[H,A,CovNil[A]](service,CovNil[A])}
+  def apply[H,A](service: Service[H,A]) ={CovCompose[H,A,CovNil[A]](service,CovNil[A])}
 }
 
 
@@ -35,22 +39,30 @@ sealed trait ServiceMatrix{
   type CovSignature[A] <: CovServiceList[A]
 }
 
-final case class ::: [H, T <: ServiceMatrix](tail : T)(service: Service[H,H], servicesColumn : tail.ContrSignature[H], servicesRow: tail.CovSignature[H] ) extends ServiceMatrix {
-  type ContrSignature[A] = ::=>[A,H,tail.ContrSignature[A]]
-  type CovSignature[A] = ::<=[H,A,tail.CovSignature[A]]
-  def enlarge[A](service: Service[A,A], servicesColumn : this.ContrSignature[A], servicesRow: this.CovSignature[A]) = {
-    :::(this)(service,servicesColumn,servicesRow)
-  }
-}
-
 object NilMatrix extends ServiceMatrix {
   type ContrSignature[A] = ContrNil[A]
   type CovSignature[A] = CovNil[A]
 }
 
+
+
+object MatrixCompose {
+  def apply[H, T <: ServiceMatrix] (tail : T)(service: Service[H,H], servicesColumn : tail.ContrSignature[H], servicesRow: tail.CovSignature[H] ) = {
+    InnerMatrixCompose(tail,service,servicesColumn,servicesRow)
+  }
+
+  protected final case class InnerMatrixCompose [H, T <: ServiceMatrix, SCONTR<: ContrServiceList[H], SCOV <: CovServiceList[H]](tail : T,service: Service[H,H], servicesColumn : SCONTR, servicesRow: SCOV ) extends ServiceMatrix {
+    type ContrSignature[A] = ContrCompose[A,H,tail.ContrSignature[A]]
+    type CovSignature[A] = CovCompose[H,A,tail.CovSignature[A]]
+    def enlarge[A](service: Service[A,A], servicesColumn : this.ContrSignature[A], servicesRow: this.CovSignature[A]) = {
+      MatrixCompose(this)(service,servicesColumn,servicesRow)
+    }
+  }
+}
+
 object ServiceMatrix {
   def apply[W](service: Service[W,W]) = {
-    :::(NilMatrix)(service,ContrNil[W], CovNil[W])
+    MatrixCompose(NilMatrix)(service,ContrNil[W], CovNil[W])
   }
 }
 
