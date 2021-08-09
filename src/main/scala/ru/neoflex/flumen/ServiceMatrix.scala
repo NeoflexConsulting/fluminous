37482e54908afbfb8cbfa2dcf773095a88ae01e1
ext_vmatveev <vmatveyev@neoflex.ru>
2021-08-09 20:04:30 +0300

    FLUMEN-1. Intermediate results

--- src/main/scala/ru/neoflex/flumen/ServiceMatrix.scala
+++ src/main/scala/ru/neoflex/flumen/ServiceMatrix.scala
@@ -1,8 +1,8 @@
 package ru.neoflex.flumen
 
-import shapeless.{HList, HNil}
+import shapeless.{ HList, HNil }
 
-sealed trait ServiceMatrix{
+sealed trait ServiceMatrix {
   type THIS <: ServiceMatrix
   type DIAGONAL_TYPE
   type SERVICE_WITH_INPUT_TYPE[I] <: ServicesWithInput[I]
@@ -10,30 +10,52 @@ sealed trait ServiceMatrix{
 }
 
 object NilServiceMatrix extends ServiceMatrix {
-  type THIS = NilServiceMatrix.type
-  type DIAGONAL_TYPE = Nothing
-  type SERVICE_WITH_INPUT_TYPE[A] = ServicesWithInputNil[A]
+  type THIS                         = NilServiceMatrix.type
+  type DIAGONAL_TYPE                = Nothing
+  type SERVICE_WITH_INPUT_TYPE[A]   = ServicesWithInputNil[A]
   type SERVICES_WITH_OUTPUT_TYPE[A] = ServicesWithOutputNil[A]
 }
 
-
 object ServiceMatrixCompose {
-  def apply[TYPE, TAIL <: ServiceMatrix] (tail : TAIL)(service: Service[TYPE,TYPE],  conditions: Seq[Condition[TYPE]], servicesWitInput : tail.SERVICE_WITH_INPUT_TYPE[TYPE], servicesWithOutput: tail.SERVICES_WITH_OUTPUT_TYPE[TYPE] ) = {
+  def apply[TYPE, TAIL <: ServiceMatrix](
+    tail: TAIL
+  )(
+    service: Service[TYPE, TYPE],
+    conditions: Seq[Condition[TYPE]],
+    servicesWitInput: tail.SERVICE_WITH_INPUT_TYPE[TYPE],
+    servicesWithOutput: tail.SERVICES_WITH_OUTPUT_TYPE[TYPE]
+  ) = {
 
-    InnerServiceMatrixCompose(tail,service,conditions, servicesWitInput,servicesWithOutput)
+    InnerServiceMatrixCompose(tail, service, conditions, servicesWitInput, servicesWithOutput)
   }
 
-  final case class InnerServiceMatrixCompose [TYPE, TAIL <: ServiceMatrix, TAIL_SERVICES_WITH_INPUT_TYPE<: ServicesWithInput[TYPE],
-      TAIL_SERVICES_WITH_OUTPUT_TYPE <: ServicesWithOutput[TYPE]](tail : TAIL,service: Service[TYPE,TYPE], conditions: Seq[Condition[TYPE]], serviceWithInput : TAIL_SERVICES_WITH_INPUT_TYPE, servicesWithOutput: TAIL_SERVICES_WITH_OUTPUT_TYPE ) extends ServiceMatrix {
-    type THIS =  InnerServiceMatrixCompose [TYPE, TAIL, TAIL_SERVICES_WITH_INPUT_TYPE, TAIL_SERVICES_WITH_OUTPUT_TYPE]
-    type DIAGONAL_TYPE = TYPE
-    type SERVICE_WITH_INPUT_TYPE[A] = ServicesWithInputCompose[A,DIAGONAL_TYPE,tail.SERVICE_WITH_INPUT_TYPE[A]]
-    type SERVICES_WITH_OUTPUT_TYPE[A] = ServicesWithOutputCompose[DIAGONAL_TYPE,A,tail.SERVICES_WITH_OUTPUT_TYPE[A]]
+  final case class InnerServiceMatrixCompose[
+    TYPE,
+    TAIL <: ServiceMatrix,
+    TAIL_SERVICES_WITH_INPUT_TYPE <: ServicesWithInput[TYPE],
+    TAIL_SERVICES_WITH_OUTPUT_TYPE <: ServicesWithOutput[TYPE]
+  ](
+    tail: TAIL,
+    service: Service[TYPE, TYPE],
+    conditions: Seq[Condition[TYPE]],
+    serviceWithInput: TAIL_SERVICES_WITH_INPUT_TYPE,
+    servicesWithOutput: TAIL_SERVICES_WITH_OUTPUT_TYPE)
+      extends ServiceMatrix {
+    type THIS                         = InnerServiceMatrixCompose[TYPE, TAIL, TAIL_SERVICES_WITH_INPUT_TYPE, TAIL_SERVICES_WITH_OUTPUT_TYPE]
+    type DIAGONAL_TYPE                = TYPE
+    type SERVICE_WITH_INPUT_TYPE[A]   = ServicesWithInputCompose[A, DIAGONAL_TYPE, tail.SERVICE_WITH_INPUT_TYPE[A]]
+    type SERVICES_WITH_OUTPUT_TYPE[A] = ServicesWithOutputCompose[DIAGONAL_TYPE, A, tail.SERVICES_WITH_OUTPUT_TYPE[A]]
 
-    def enlarge[A](servicesColumn : this.SERVICE_WITH_INPUT_TYPE[A], servicesRow: this.SERVICES_WITH_OUTPUT_TYPE[A], service: Service[A,A], conditions: Seq[Condition[A]] = Seq.empty[Condition[A]]) = {
-      ServiceMatrixCompose(this)(service,conditions,servicesColumn,servicesRow)
+    def enlarge[NEXT_TYPE](
+      nextservicesWithInput: this.SERVICE_WITH_INPUT_TYPE[NEXT_TYPE],
+      nextServicesWithOutput: this.SERVICES_WITH_OUTPUT_TYPE[NEXT_TYPE],
+      services: Service[NEXT_TYPE, NEXT_TYPE],
+      conditions: Seq[Condition[NEXT_TYPE]] = Seq.empty[Condition[NEXT_TYPE]]
+    ) = {
+
+      ServiceMatrixCompose(this)(services, conditions, nextservicesWithInput, nextServicesWithOutput)
     }
-    def get[I,O](implicit get: MatrixGet[THIS,I,O]):Service[I,O] = get(this)
+    def get[I, O](implicit get: MatrixGet[I, O, THIS]): Service[I, O] = get(this)
 
     // rows = гетерогенный список совариантных сервисов, у которых первый параметры имеет вид Service[H,A]
     /*   protected def appendRuntimeData[R<:HList, T<: HList](runtime: R, rows: Seq[CovServiceList[]] ) = {
@@ -41,12 +63,12 @@ object ServiceMatrixCompose {
        }*/
 
     def toRouterRuntime(): HList = {
-      val services = this.serviceWithInput.append(this.service)
+      val services    = this.serviceWithInput.append(this.service)
       val runtimeData = TypeInfo[TYPE](Seq.empty[Variable[TYPE]], services, this.conditions)
-      val runtime = runtimeData :: HNil
+      val runtime     = runtimeData :: HNil
       //Надо из serviceRows сделать HList контрвариантных списков
       // val columns = this.servicesRow.toHList
-      (1 ::232 :: "strtr" :: HNil)
+      (1 :: 232 :: "strtr" :: HNil)
       val runtimeConstructor = new RuntimeConstructor(runtime)
       ???
     }
@@ -54,10 +76,15 @@ object ServiceMatrixCompose {
 }
 
 object ServiceMatrix {
-  def apply[W](service: Service[W,W], condition: Condition[W]) = {
-    ServiceMatrixCompose(NilServiceMatrix)(service,Seq(condition), ServicesWithInputNil[W], ServicesWithOutputNil[W])
+  def apply[W](service: Service[W, W], condition: Condition[W]) = {
+    ServiceMatrixCompose(NilServiceMatrix)(service, Seq(condition), ServicesWithInputNil[W], ServicesWithOutputNil[W])
   }
-  def apply[W](service: Service[W,W]) = {
-    ServiceMatrixCompose(NilServiceMatrix)(service,Seq.empty[Condition[W]], ServicesWithInputNil[W], ServicesWithOutputNil[W])
+  def apply[W](service: Service[W, W]) = {
+    ServiceMatrixCompose(NilServiceMatrix)(
+      service,
+      Seq.empty[Condition[W]],
+      ServicesWithInputNil[W],
+      ServicesWithOutputNil[W]
+    )
   }
 }
--- src/main/scala/ru/neoflex/flumen/ServiceRouter.scala
+++ src/main/scala/ru/neoflex/flumen/ServiceRouter.scala
@@ -1,66 +1,71 @@
 package ru.neoflex.flumen
 
 import ru.neoflex.flumen.ServiceMatrixCompose.InnerServiceMatrixCompose
-import shapeless.{HList, HNil}
 class NotFoundException extends Exception
 
 // TODO Implement Unique typeclasses
 
-
-
-
-
-//Класс типов, Умеет для матрицы M возвращать Service[A,U]
-trait MatrixGet[M <: ServiceMatrix,I,O] {
-  def apply(t: M): Service[I,O]
+trait MatrixGet[I, O, M <: ServiceMatrix] {
+  def apply(t: M): Service[I, O]
 }
 object MatrixGet {
-  def apply[M <: ServiceMatrix,I,O](implicit got: MatrixGet[M,I,O]): MatrixGet[M,I,O] = got
-  //У нас 3 базовых случая
-  //Когда у нас M <: ServiceMatrix,I,O и есть implicit ContrGet[M.ContrSignature, I,O]
-  implicit def selectContrvariant[M <: ServiceMatrix,CONTR<: ServicesWithInput[I], COV <: ServicesWithOutput[I],I,O](implicit c: GetServiceByOutput[I,O,CONTR]):
-  MatrixGet[InnerServiceMatrixCompose[I,M,CONTR,COV],I,O] = {
-    new MatrixGet[InnerServiceMatrixCompose[I,M,CONTR,COV],I,O] {
-      override def apply(t: InnerServiceMatrixCompose[I, M, CONTR, COV]): Service[I, O] = c(t.serviceWithInput)
+  def apply[I, O, M <: ServiceMatrix](implicit got: MatrixGet[I, O, M]): MatrixGet[I, O, M] = got
+
+  implicit def selectContrvariant[
+    I,
+    O,
+    TM <: ServiceMatrix,
+    CONTR <: ServicesWithInput[I],
+    COV <: ServicesWithOutput[I]
+  ](implicit c: GetServiceByOutput[I, O, CONTR]
+  ): MatrixGet[I, O, InnerServiceMatrixCompose[I, TM, CONTR, COV]] = {
+    new MatrixGet[I, O, InnerServiceMatrixCompose[I, TM, CONTR, COV]] {
+      override def apply(t: InnerServiceMatrixCompose[I, TM, CONTR, COV]): Service[I, O] = c(t.serviceWithInput)
     }
   }
   //Когда у нас M <: ServiceMatrix,I,O и есть implicit CovGet[M.CovSignature, I,O]
-  implicit def selectCovariant[M <: ServiceMatrix,CONTR<: ServicesWithInput[O], COV <: ServicesWithOutput[O],I,O](implicit c: GetServiceByInput[I,O,COV]):
-  MatrixGet[InnerServiceMatrixCompose[O,M,CONTR,COV],I,O] = new MatrixGet[InnerServiceMatrixCompose[O,M,CONTR,COV],I,O]{
-    def apply(t : InnerServiceMatrixCompose[O,M,CONTR,COV]):Service[I,O] = c(t.servicesWithOutput)
-  }
+  implicit def selectCovariant[I, O, TM <: ServiceMatrix, CONTR <: ServicesWithInput[O], COV <: ServicesWithOutput[O]](
+    implicit c: GetServiceByInput[I, O, COV]
+  ): MatrixGet[I, O, InnerServiceMatrixCompose[O, TM, CONTR, COV]] =
+    new MatrixGet[I, O, InnerServiceMatrixCompose[O, TM, CONTR, COV]] {
+      def apply(t: InnerServiceMatrixCompose[O, TM, CONTR, COV]): Service[I, O] = c(t.servicesWithOutput)
+    }
 
   //Когда у нас M <: ServiceMatrix,M.Diagonal,M.diagonal
-  implicit def selectDiagonal[M <: ServiceMatrix,H, CONTR<: ServicesWithInput[H], COV <: ServicesWithOutput[H]]:
-  MatrixGet[InnerServiceMatrixCompose[H,M,CONTR,COV],H,H] =
-    new MatrixGet[InnerServiceMatrixCompose[H,M,CONTR,COV],H,H] {
-      def apply(t : InnerServiceMatrixCompose[H,M,CONTR,COV]):Service[H,H] = t.service
+  implicit def selectDiagonal[
+    M <: ServiceMatrix,
+    H,
+    CONTR <: ServicesWithInput[H],
+    COV <: ServicesWithOutput[H]
+  ]: MatrixGet[H, H, InnerServiceMatrixCompose[H, M, CONTR, COV]] =
+    new MatrixGet[H, H, InnerServiceMatrixCompose[H, M, CONTR, COV]] {
+      def apply(t: InnerServiceMatrixCompose[H, M, CONTR, COV]): Service[H, H] = t.service
     }
 
   //И рекурсивный случай, перебирающий матрицы
-  implicit def recurse[M <: ServiceMatrix,H, CONTR<: ServicesWithInput[H], COV <: ServicesWithOutput[H], I,O]
-  (implicit mt : MatrixGet[M, I, O]): MatrixGet[InnerServiceMatrixCompose[H,M,CONTR,COV],I,O] =
-    new MatrixGet[InnerServiceMatrixCompose[H,M,CONTR,COV],I,O] {
-      def apply(t :InnerServiceMatrixCompose[H,M,CONTR,COV]):Service[I,O]  = mt(t.tail)
+  implicit def recurse[M <: ServiceMatrix, H, CONTR <: ServicesWithInput[H], COV <: ServicesWithOutput[H], I, O](
+    implicit mt: MatrixGet[I, O, M]
+  ): MatrixGet[I, O, InnerServiceMatrixCompose[H, M, CONTR, COV]] =
+    new MatrixGet[I, O, InnerServiceMatrixCompose[H, M, CONTR, COV]] {
+      def apply(t: InnerServiceMatrixCompose[H, M, CONTR, COV]): Service[I, O] = mt(t.tail)
     }
 }
 
-
 object Test {
   def main(args: Array[String]): Unit = {
-    val upperCaseService = Service[String,String]("upper",_.toUpperCase)
-    val incrementService = Service[Int,Int]("increment",_ + 1)
-    val toStringService = Service[Int, String]("to_string",_.toString)
-    val toIntService = Service[String,Int]("to_int",_.toInt)
+    val upperCaseService = Service[String, String]("upper", _.toUpperCase)
+    val incrementService = Service[Int, Int]("increment", _ + 1)
+    val toStringService  = Service[Int, String]("to_string", _.toString)
+    val toIntService     = Service[String, Int]("to_int", _.toInt)
 
     val serviceMatrix = ServiceMatrix(upperCaseService)
-    val bigServiceMatrix = serviceMatrix.
-          enlarge(ServicesWithInput(toStringService), ServicesWithOutput(toIntService),incrementService)
+    val bigServiceMatrix =
+      serviceMatrix.enlarge(ServicesWithInput(toStringService), ServicesWithOutput(toIntService), incrementService)
 
-    println(serviceMatrix.get[String,String].invoke("some String from Service Matrix1"))
-    println(bigServiceMatrix.get[Int,Int].invoke(3))
-    println(bigServiceMatrix.get[Int,String].invoke(3))
-    println(bigServiceMatrix.get[String,Int].invoke("3"))
-    println(bigServiceMatrix.get[String,String].invoke("some String"))
+    println(serviceMatrix.get[String, String].invoke("some String from Service Matrix1"))
+    println(bigServiceMatrix.get[Int, Int].invoke(3))
+    println(bigServiceMatrix.get[Int, String].invoke(3))
+    println(bigServiceMatrix.get[String, Int].invoke("3"))
+    println(bigServiceMatrix.get[String, String].invoke("some String"))
   }
 }
