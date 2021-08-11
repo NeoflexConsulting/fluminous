2b218e83229c62d22e1384c61a1e205b48b84780
vmatveyev <vmatveyev@neoflex.ru>
2021-08-12 00:46:47 +0300

    FLUMEN-1. Modified test example

--- src/main/scala/org/fluminous/matrix/ServicesWithOutput.scala
+++ src/main/scala/org/fluminous/matrix/ServicesWithOutput.scala
@@ -2,18 +2,13 @@ package org.fluminous.matrix
 
 sealed trait ServicesWithOutput[O] {
   type THIS <: ServicesWithOutput[O]
-  type L2D <: ServicesWithInput2DList
   def append[I](service: Service[I, O]): ServicesWithOutputCompose[I, O, THIS]
-  def toServices2DList: L2D
-
 }
 
 sealed class ServicesWithOutputNil[O] extends ServicesWithOutput[O] {
   type THIS = ServicesWithOutputNil[O]
-  type L2D  = ServicesWithInput2DNil.type
   override def append[I](service: Service[I, O]): ServicesWithOutputCompose[I, O, THIS] =
     ServicesWithOutputCompose(service, ServicesWithOutputNil[O])
-  override def toServices2DList: L2D = ServicesWithInput2DNil
 }
 
 object ServicesWithOutputNil {
@@ -23,15 +18,9 @@ object ServicesWithOutputNil {
 final case class ServicesWithOutputCompose[I, O, TAIL <: ServicesWithOutput[O]](service: Service[I, O], tail: TAIL)
     extends ServicesWithOutput[O] {
   type THIS = ServicesWithOutputCompose[I, O, TAIL]
-  type L2D  = ServicesWithInput2DCompose[I, ServicesWithInputCompose[I, O, ServicesWithInputNil[I]], tail.L2D]
-
   override def append[NEXT_INPUT](service: Service[NEXT_INPUT, O]): ServicesWithOutputCompose[NEXT_INPUT, O, THIS] =
     ServicesWithOutputCompose(service, this)
-
   def getByInput[SEARCHED](implicit get: GetServiceByInput[SEARCHED, O, THIS]): Service[SEARCHED, O] = get.apply(this)
-
-  override def toServices2DList: L2D =
-    ServicesWithInput2DCompose(ServicesWithInput(service), tail.toServices2DList)
 }
 
 object ServicesWithOutput {
