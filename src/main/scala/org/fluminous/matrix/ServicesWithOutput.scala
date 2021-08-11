682f003bfd97e2e61de793f7d561ebbcd85826e2
vmatveyev <vmatveyev@neoflex.ru>
2021-08-11 20:21:00 +0300

    FLUMEN-1. Implemented appendColumn

--- src/main/scala/org/fluminous/matrix/ServicesWithOutput.scala
+++ src/main/scala/org/fluminous/matrix/ServicesWithOutput.scala
@@ -3,25 +3,17 @@ package org.fluminous.matrix
 sealed trait ServicesWithOutput[O] {
   type THIS <: ServicesWithOutput[O]
   type L2D <: ServicesWithInput2DList
-  type APPENDED_L2D <: ServicesWithInput2DList
-  type L2D_SCHEME[A <: CONSTRAINT_TYPE] <: ServicesWithInput2DList
-  type CONSTRAINT_TYPE
   def append[I](service: Service[I, O]): ServicesWithOutputCompose[I, O, THIS]
-  def appendTo2DList[L <: CONSTRAINT_TYPE](d2List: L2D_SCHEME[L]): APPENDED_L2D
   def toServices2DList: L2D
 
 }
 
 sealed class ServicesWithOutputNil[O] extends ServicesWithOutput[O] {
-  type THIS                             = ServicesWithOutputNil[O]
-  type L2D                              = ServicesWithInput2DNil.type
-  type APPENDED_L2D                     = ServicesWithInput2DNil.type
-  type L2D_SCHEME[A <: CONSTRAINT_TYPE] = ServicesWithInput2DNil.type
-  type CONSTRAINT_TYPE                  = ServicesWithInput[_]
+  type THIS = ServicesWithOutputNil[O]
+  type L2D  = ServicesWithInput2DNil.type
   override def append[I](service: Service[I, O]): ServicesWithOutputCompose[I, O, THIS] =
     ServicesWithOutputCompose(service, ServicesWithOutputNil[O])
-  override def appendTo2DList[L <: CONSTRAINT_TYPE](d2List: L2D_SCHEME[L]): APPENDED_L2D = ServicesWithInput2DNil
-  override def toServices2DList: L2D                                                     = ServicesWithInput2DNil
+  override def toServices2DList: L2D = ServicesWithInput2DNil
 }
 
 object ServicesWithOutputNil {
@@ -31,20 +23,12 @@ object ServicesWithOutputNil {
 final case class ServicesWithOutputCompose[I, O, TAIL <: ServicesWithOutput[O]](service: Service[I, O], tail: TAIL)
     extends ServicesWithOutput[O] {
   type THIS = ServicesWithOutputCompose[I, O, TAIL]
-  type L2D  = L2D_SCHEME[ServicesWithInputCompose[I, O, ServicesWithInputNil[I]]]
-  type APPENDED_L2D =
-    ServicesWithInput2DCompose[I, ServicesWithInputCompose[I, O, L2D#LIST], tail.APPENDED_L2D]
-  type L2D_SCHEME[A <: CONSTRAINT_TYPE] = ServicesWithInput2DCompose[I, A, tail.L2D]
-  type CONSTRAINT_TYPE                  = ServicesWithInput[I]
+  type L2D  = ServicesWithInput2DCompose[I, ServicesWithInputCompose[I, O, ServicesWithInputNil[I]], tail.L2D]
 
   override def append[NEXT_INPUT](service: Service[NEXT_INPUT, O]): ServicesWithOutputCompose[NEXT_INPUT, O, THIS] =
     ServicesWithOutputCompose(service, this)
+
   def getByInput[SEARCHED](implicit get: GetServiceByInput[SEARCHED, O, THIS]): Service[SEARCHED, O] = get.apply(this)
-  override def appendTo2DList[L <: ServicesWithInput[I]](d2List: L2D_SCHEME[L]): APPENDED_L2D = {
-    /*val ds = ServicesWithInputCompose(service, d2List.services)
-    ServicesWithInput2DCompose(ds, tail.appendTo2DList(d2List.tail))*/
-    ???
-  }
 
   override def toServices2DList: L2D =
     ServicesWithInput2DCompose(ServicesWithInput(service), tail.toServices2DList)
