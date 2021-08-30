db7cefc4c3ce1e77c6bc95d85b9cfa8646d3e9e3
vmatveyev <vmatveyev@neoflex.ru>
2021-08-30 14:59:50 +0300

    FLUMEN-1. Added selector filter

--- src/main/scala/org/fluminous/jq/filters/Filter.scala
+++ src/main/scala/org/fluminous/jq/filters/Filter.scala
@@ -3,7 +3,8 @@ package org.fluminous.jq.filters
 import org.fluminous.jq.Expression
 
 trait Filter extends Expression {
-  def matches(stack: List[Expression]): Boolean                     = patterns.isDefinedAt(stack)
-  def instantiateOnStack(stack: List[Expression]): List[Expression] = patterns.lift(stack).getOrElse(stack)
-  protected val patterns: PartialFunction[List[Expression], List[Expression]]
+  def isSuitableForStack(stack: List[Expression]): Boolean          = FilterStartCases(stack)
+  def instantiateOnStack(stack: List[Expression]): List[Expression] = FilterCases.lift(stack).getOrElse(stack)
+  protected val FilterStartCases: List[Expression] => Boolean
+  protected val FilterCases: PartialFunction[List[Expression], List[Expression]]
 }
--- src/main/scala/org/fluminous/jq/filters/Selector.scala
+++ src/main/scala/org/fluminous/jq/filters/Selector.scala
@@ -4,7 +4,15 @@ import org.fluminous.jq.Expression
 import org.fluminous.jq.tokens.{ Identifier, RawString, Root }
 
 final case class Selector(path: Seq[String]) extends Filter {
-  override val patterns: PartialFunction[List[Expression], List[Expression]] = {
+
+  override val FilterStartCases: List[Expression] => Boolean = {
+    case Nil              => true
+    case Root :: _        => true
+    case Selector(_) :: _ => true
+    case _                => false
+  }
+
+  override val FilterCases: PartialFunction[List[Expression], List[Expression]] = {
     case Identifier(value) :: Root :: rest                   => Selector(Seq(value)) :: rest
     case RawString(value, _) :: Root :: rest                 => Selector(Seq(value)) :: rest
     case Selector(pathChild) :: Selector(pathParent) :: rest => Selector(pathParent ++ pathChild) :: rest
