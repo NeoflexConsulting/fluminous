1abfd560f0144a1a18c015a59434d0752296e5cf
vmatveyev <vmatveyev@neoflex.ru>
2021-09-17 18:16:42 +0300

    FLUMINOUS-2. Working on error processing

--- src/main/scala/org/fluminous/jq/input/InputProvider.scala
+++ src/main/scala/org/fluminous/jq/input/InputProvider.scala
@@ -11,7 +11,7 @@ private final case class NullProvider(override val position: Int) extends InputP
   override def moveToNext(): InputProvider = this
 }
 
-private final case class StringProvider(input: String, override val position: Int = 0) extends InputProvider {
+private final case class StringProvider(input: String, override val position: Int = 1) extends InputProvider {
   override def nextSymbol: Symbol = input.headOption.fold[Symbol](EOF)(Character)
   override def moveToNext(): InputProvider =
     input.headOption.fold[InputProvider](NullProvider(position))(_ => StringProvider(input.substring(1), position + 1))
