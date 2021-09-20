0f8f24959655a2dbdaccd92545a2553b1a275a97
vmatveyev <vmatveyev@neoflex.ru>
2021-09-20 15:18:51 +0300

    FLUMINOUS-2. Fixed test

--- src/test/scala/org/fluminous/jq/ErrorSpec.scala
+++ src/test/scala/org/fluminous/jq/ErrorSpec.scala
@@ -5,8 +5,8 @@ import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpecLike
 
 class ErrorSpec extends AnyWordSpecLike with Matchers with BeforeAndAfterAll with Parser {
-  "Filters" should {
-    "select from json" in {
+  "Parsing of wrong jq statements" should {
+    "fail with error" in {
       parse("{customerId, body : {customer: {id : customerId, name, age}} }") should be (
         Left(ParserException(38,"Error occurred while parsing json object: Unexpected customerId"))
       )
