5cde856163773eb01d0fcdf8a44f7c8c1001fced
vmatveyev <vmatveyev@neoflex.ru>
2021-10-18 12:36:08 +0300

    FLUMINOUS-3. Transform now works with list of jsons

--- src/main/scala/org/fluminous/jq/filter/selector/Splitter.scala
+++ src/main/scala/org/fluminous/jq/filter/selector/Splitter.scala
@@ -4,19 +4,28 @@ import io.circe.Json
 import org.fluminous.jq.{ Description, EvaluationException }
 import org.fluminous.jq.filter.Filter
 
-case class Splitter(override val position: Int) extends Filter {
+case class Splitter(override val position: Int, parentFieldName: Option[String] = None)
+    extends Filter
+    with SelectorFunctions {
   override val isSingleValued: Boolean = false
 
   override def transform(input: Json): Either[EvaluationException, List[Json]] = {
-    input.asArray
-      .map(_.toList)
-      .orElse(input.asObject.map(_.toList.map(_._2)))
-      .toRight(
-        EvaluationException(
-          position,
-          s"Trying to split by elements json of type ${input.name}"
-        )
-      )
+    if (input.isNull) {
+      Right(List(input))
+    } else {
+      for {
+        childJson <- jsonFromFieldName(input, parentFieldName)
+        result <- childJson.asArray
+                   .map(_.toList)
+                   .orElse(input.asObject.map(_.toList.map(_._2)))
+                   .toRight(
+                     EvaluationException(
+                       position,
+                       s"Trying to split by elements json of type ${input.name}"
+                     )
+                   )
+      } yield result
+    }
   }
   override val description: String = s"splitter"
 }
